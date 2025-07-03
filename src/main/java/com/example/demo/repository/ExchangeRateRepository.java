fcec8ddbcca7a68d14a330c0bd7c86ce44119b84
ryckutis <ricardasgr225@gmail.com>
2025-07-03 09:00:39 +0300

    update ExchangeRateRepository

--- src/main/java/com/example/demo/repository/ExchangeRateRepository.java
+++ src/main/java/com/example/demo/repository/ExchangeRateRepository.java
@@ -4,8 +4,11 @@ import com.example.demo.model.ExchangeRate;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 public interface ExchangeRateRepository extends JpaRepository<ExchangeRate, Long> {
     Optional<ExchangeRate> findByBaseCurrencyAndTargetCurrencyAndDate(String baseCurrency, String targetCurrency, LocalDate date);
+
+    List<ExchangeRate> findByBaseCurrencyAndTargetCurrencyAndDateBetween(String baseCurrency, String targetCurrency, LocalDate startDate, LocalDate endDate);
 }
